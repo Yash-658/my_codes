36b6915985d7593d9759f18669178ade9a402f2e
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-07-10 12:44:25 +0530

    .

--- C++/C++/Queue/intro.cpp
+++ C++/C++/Queue/intro.cpp
@@ -22,3 +22,46 @@ using namespace std;
 // return 0; 
 // }
 
+
+#include <bits/stdc++.h> 
+class Queue {
+    int *arr;
+    int _front;
+    int rear;
+
+public:
+    Queue() {
+        arr = new int[5001];
+        _front = 0;
+        rear = 0;                               // you can also try to implement isempty()
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty() {
+        if(_front == rear) return true;
+        else return false;
+    }
+
+    void enqueue(int data) {
+        arr[rear] = data;
+        rear++;
+    }
+
+    int dequeue() {
+        if(isEmpty()) return -1;
+
+        int temp = arr[_front];    // to avoid memory wastage, now we will apply this concept that if the array becomes empty after popping an element,
+        _front++;                  // that means front == rear, then we will make front = rear = 0 again.
+        if(isEmpty()) {
+            _front = 0;
+            rear = 0;
+        }
+        return temp;
+    }
+
+    int front() {
+        if(isEmpty()) return -1;
+        return arr[_front];
+    }
+};

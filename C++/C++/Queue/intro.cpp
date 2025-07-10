124333e776bc2ac076cd63198909ee625328e7d8
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-07-10 12:16:09 +0530

    adding queue

---
+++ C++/C++/Queue/intro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    q.push(8);
+    q.push(9);
+
+    while(!q.empty()) {
+        cout<<q.front();
+        q.pop();
+    }
+    
+return 0;
+}

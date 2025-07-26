29ed443fdfb0375bf8f78b1c9f0866c719b31714
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-07-26 22:57:45 +0530

    .

---
+++ C++/C++/Hashmap/Intro.cpp
@@ -0,0 +1,32 @@
+// Ordered map uses a red-black tree (type of a self balancing BST) to give O(logn) TCs for insertion, deletion and searching~
+// Unordered map uses hash-table and have amortized TC of O(1) in insertion, deletiong, searching etc
+
+// when we try to access a key which doesn't exist in the map with [], it creates the respective key with value "0"~
+// but when we try to access the unexisting key with ".at()", it gives an exception~
+
+// count() fxn is used to check if a given key exists or not, returns 1 if present and 0 if absent
+// erase() to delete a pair by its "key"
+
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string, int> map;
+    pair<string, int> temp = {"yash", 10};
+    map.insert(temp);
+    map["aryan"] = 7;
+    map.insert({"vivek", 4});
+
+    unordered_map<string, int> :: iterator it = map.begin();        
+    while(it != map.end()) {                                    // to traverse the whole map: we can either use for(auto i: map)
+        cout<<it->first<<" "<<it->second<<endl;                 // OR we can use a iterator~
+        it++;
+    }
+
+return 0;
+}

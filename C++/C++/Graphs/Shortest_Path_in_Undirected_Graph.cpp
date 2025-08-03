dec174d0a911c45596bc93c23d70ae22a6aa05f9
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-03 15:04:57 +0530

    .

---
+++ C++/C++/Graphs/Shortest_Path_in_Undirected_Graph.cpp
@@ -0,0 +1,40 @@
+// This is using BFS
+// vector<int> shortestPath( vector<pair<int,int>> edges , int n , int m, int s , int t){
+	
+// 	vector<vector<int>> adj(n+1);
+// 	for(const auto &pair: edges) {
+// 		adj[pair.first].push_back(pair.second);
+// 		adj[pair.second].push_back(pair.first);
+// 	}
+
+// 	vector<bool> visited(n+1, false);
+// 	vector<int> child_parent(n+1);
+
+// 	queue<int> q;
+// 	q.push(s);
+// 	visited[s] = true;
+// 	child_parent[s] = -1;
+
+// 	while(!q.empty()) {
+// 		int temp = q.front();
+// 		q.pop();
+
+// 		for(const auto i: adj[temp]) {
+// 			if(!visited[i]) {
+// 				visited[i] = true;
+// 				child_parent[i] = temp;
+// 				q.push(i);
+// 			}
+// 		}
+// 	}
+
+// 	// prepare shortest path
+// 	vector<int> ans;
+// 	int curr = t;
+// 	while(curr != -1) {
+//      ans.push_back(curr);
+// 		curr = child_parent[curr];
+// 	}
+// 	reverse(ans.begin(), ans.end());
+// 	return ans;
+// }

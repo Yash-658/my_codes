89bbf0bce9f813a53ba3e833d3cbbb34b3db0530
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-08 12:16:42 +0530

    .

---
+++ C++/C++/Graphs/MST_PrimsAlgo.cpp
@@ -0,0 +1,3 @@
+// MST(Minimum Spanning Tree) is a Spanning Tree which uses cheapest set of weights of edges(sum of all weights is minm)~
+// Spanning Tree: It's a tree which: 1) uses exactly "n-1" edges (if you have "n" nodes)
+//                                   2) each node is reachable by every other node

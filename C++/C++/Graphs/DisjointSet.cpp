e2529f30dd70141eb730bec9a8a5049adc21786e
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-13 19:57:12 +0530

    .

--- C++/C++/Graphs/DisjointSet.cpp
+++ C++/C++/Graphs/DisjointSet.cpp
@@ -1,2 +1,71 @@
+// Disjoint Set are used in dynamic graphs (used to tell if two nodes are in the same component in constant time)
+
 // UNION BY RANK
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class DisjointSet {
+    vector<int> rank, parent;
+public:
+    DisjointSet(int n) {
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);     // path compression~
+    }
+
+    void unionByRank(int u, int v) {
+        int Ult_u = findUPar(u);                         // find ultimate parent of both u and v
+        int Ult_v = findUPar(v);
+
+        if(Ult_u == Ult_v) return;                       // if they belong to the same component, we don't have to do anything
+
+        int rankU = rank[Ult_u];                         // find rank of ultimate parent of both u and v
+        int rankV = rank[Ult_v];                         
+
+        if(rankU == rankV) {                                
+            // can add any one to anyone as ranks are equal~
+            parent[Ult_u] = Ult_v;
+            rank[Ult_v]++;
+        }
+
+        else if(rankU > rankV) {
+            parent[Ult_v] = Ult_u;
+        }
+
+        else {
+            parent[Ult_u] = Ult_v;
+        }
+    }
+};
+
+int main()
+{
+    DisjointSet Ds(7);
+    Ds.unionByRank(1, 2);
+    Ds.unionByRank(2, 3);
+    Ds.unionByRank(4, 5);
+    Ds.unionByRank(6, 7);
+    Ds.unionByRank(5, 6);
+
+    if(Ds.findUPar(3) == Ds.findUPar(7)) cout<<"Same Component of 3 and 7"<<endl;           // if Ult parent of both nodes are same, they are in the same Component
+    else cout<<"Different Component of 3 and 7"<<endl;
+    if(Ds.findUPar(5) == Ds.findUPar(7)) cout<<"Same Component of 5 and 7"<<endl;
+    else cout<<"Different Component of 5 and 7"<<endl;
+
+    return 0;
+}
+
 

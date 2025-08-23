d5617c6d49e0b545fbde3538be5ddc4a9c31d579
Yash Raghuwanshi <136227872+darkie198@users.noreply.github.com>
2025-08-23 23:06:03 +0530

    .

---
+++ C++/C++/Graphs/problems/identical_islands.cpp
@@ -0,0 +1,50 @@
+// https://www.geeksforgeeks.org/problems/number-of-distinct-islands/1
+
+// The trick was to know how to store the shape of the island~
+
+// class Solution {
+//         void new_island(int x, int y, int m, int n, string &shape, vector<vector<int>>& grid) {
+//         // mark it as visited by turning connected 1s to 0
+//         grid[x][y] = 0;
+
+//         static int dx[4] = {-1, 1, 0, 0};
+//         static int dy[4] = {0, 0, -1, 1};
+//         static string dir = "UDLR";
+
+//         for(int i = 0; i < 4; i++) {
+//             int new_x = x + dx[i];
+//             int new_y = y + dy[i];
+
+//             if(new_x >= 0 && new_x < m && new_y >= 0 && new_y < n && grid[new_x][new_y] == 1) {
+//                 shape.push_back(dir[i]);
+//                 new_island(new_x, new_y, m, n, shape, grid);
+//             }
+//         }
+        
+//         shape.push_back('B');
+//     }
+    
+//   public:
+//     int countDistinctIslands(vector<vector<int>>& grid) {
+//         int m = grid.size(), n = grid[0].size();
+//         int islands = 0;
+//         unordered_set<string> shapes;
+        
+//         for(int i = 0; i < m; i++) {
+//             for(int j = 0; j < n; j++) {
+//                 if(grid[i][j] == 1) {
+//                     string shape;
+//                     new_island(i, j, m, n, shape, grid);
+                    
+//                     // now check if this is an island of new shape~
+//                     if(shapes.find(shape) == shapes.end()) {
+//                         islands++;
+//                         shapes.insert(shape);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return islands;
+//     }
+// };

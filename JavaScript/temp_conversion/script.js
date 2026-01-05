0688c3bc4bdeb6b2d64fb7333fe2a44ee7807864
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-05 22:59:50 +0530

    .

--- JavaScript/temp_conversion/script.js
+++ JavaScript/temp_conversion/script.js
@@ -6,6 +6,7 @@ const resultField = document.getElementById('result');
 console.log(inputField.value);
 
 let convertedValue;
+let symbol;
 function convert() {
     if(inputField.value == "") {
         window.alert("Please enter an appropriate value to convert"); 
@@ -14,15 +15,17 @@ function convert() {
 
     if(cel_to_far.checked){
         convertedValue = (Number(inputField.value)*1.8)+32;
+        symbol = '°F';
     }
 
     else if(far_to_cel.checked){
         convertedValue = (Number(inputField.value)-32)*(0.55);
+        symbol = '°C';
     }
 
     else{
         window.alert("Please select one of the options.")
     }
 
-    resultField.textContent = `The Result is: ${convertedValue.toFixed(2)}`;
+    resultField.textContent = `The Result is: ${convertedValue.toFixed(2)+symbol}`;
 }

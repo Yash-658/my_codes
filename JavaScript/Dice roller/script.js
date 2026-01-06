d989fa48d8f6f691524d0149e08be119b11104c5
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-06 14:23:46 +0530

    .

--- JavaScript/Dice roller/script.js
+++ JavaScript/Dice roller/script.js
@@ -3,17 +3,12 @@ const display = document.getElementById('displayArea');
 
 function roll() {
     let input = Number(inputArea.value);
-    let randoms = [];
+    let result = ``;
     while(input > 0) {
         let random = Math.floor(Math.random()*6)+1;
-        randoms.push(random);
+        result += `<img class='result' src="images/${random}.png">`;
         input--;
     }
 
-    let result = ``;
-    for(let random of randoms) {
-        result += `<img class='result' src="images/${random}.png">`;
-    }
-    console.log(result);
     display.innerHTML = result;
 }

6056848eb3768c45ef7e3fa1721a63e29f156309
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-06 14:26:59 +0530

    .

--- JavaScript/Dice roller/script.js
+++ JavaScript/Dice roller/script.js
@@ -3,6 +3,11 @@ const display = document.getElementById('displayArea');
 
 function roll() {
     let input = Number(inputArea.value);
+    if(input<=0) {
+        window.alert("Please enter a number greater than 0!");
+        return;
+    }
+    
     let result = ``;
     while(input > 0) {
         let random = Math.floor(Math.random()*6)+1;

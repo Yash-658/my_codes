2b3bfb4b32873868f0a026875674dcb37834f8b6
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-07 20:37:20 +0530

    .

---
+++ JavaScript/Calculator/script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById('display');
+let exp = "";
+
+function appendToDisplay(value) {
+    if(value === "del") {
+        exp = exp.slice(0,-1);
+    }
+
+    else if(value === "CE") {
+        exp = "";
+    }
+
+    else if(value === "=") {
+        try{
+            exp = eval(exp);
+        }
+
+        catch(error) {
+            console.error(error);
+        }
+    }
+
+    else {
+        let lastChar = exp[exp.length-1];
+        const arr = "+-*/.";
+
+        if(!(arr.includes(lastChar) && arr.includes(value))){
+            exp += value;
+        }
+    }
+
+    display.value = exp;
+}

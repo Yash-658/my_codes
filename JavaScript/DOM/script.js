af0547145e3bc256804aaf799250641f4222d5e9
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-09 10:42:13 +0530

    .

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -1,13 +1,14 @@
-// Element selectors = Methods used to target and manipulate HTML elements, they allow you to select one or multiple HTML elements
-//                     they allow you to select one or multiple elements from the DOM
+/* Element selectors = Methods used to target and manipulate HTML elements, they allow you to select one or multiple HTML elements
+                    they allow you to select one or multiple elements from the DOM
 
-// 1. document.getElementById()       // ELEMENT OR NULL
-// 2. document.getElementsByClassName() // HTML COLLECTION
-// 3. document.getElementsByTagName() // HTML COLLECTION
-// 4. docuement.querySelector()       // ELEMENT OR NULL     
-// 5. document.querySelectorAll()     // NODELIST
+1. document.getElementById()       // ELEMENT OR NULL
+2. document.getElementsByClassName() // HTML COLLECTION
+3. document.getElementsByTagName() // HTML COLLECTION
+4. docuement.querySelector()       // ELEMENT OR NULL     
+5. document.querySelectorAll()     // NODELIST
+*/
 
-const fruits = document.getElementsByClassName("fruits");
+// const fruits = document.getElementsByClassName("fruits");
 
 // console.log(fruits);
 
@@ -31,6 +32,35 @@ for(let li of listItems) {
     li.style.backgroundColor = "orange";
 }
 
-// querySelector and querySelectorAll ka dekhlo ek baar aur https://youtu.be/lfmg-EJ8gm4?si=9KhyHYFb1wwivPQk, 
+// querySelector and querySelectorAll ka dekhlo ek baar aur https://youtu.be/lfmg-EJ 8gm4?si=9KhyHYFb1wwivPQk, 
 // basically querySelectorAll returns a NodeList which is different from an HTML COLLECTION as it has built-in methods like forEach() and 
 // is static, that means its an snapshot of the DOM and HTML collections are live(The collection automatically updates if the DOM changes)
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------
+
+/* DOM NAVIGATION: Process of navigating through the structure of an HTML document using JS.
+
+.firstElementChild
+.lastElementChild
+.nextElementSibling
+.previousElementSibling
+.parentElement
+.children
+
+*/
+
+const ulELements = document.querySelectorAll("ul");
+
+ulELements.forEach(ulElement => {
+    ulElement.firstElementChild.style.backgroundColor= "pink";
+})
+
+const fruits = document.getElementById("fruits");
+fruits.lastElementChild.style.backgroundColor = "lightgreen";
+
+const desserts = document.getElementById("desserts");
+
+
+
+
+

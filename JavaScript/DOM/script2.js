94df4caa3e0aa9ed54a755016c2e78b2973b02ca
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-09 15:49:57 +0530

    .

--- JavaScript/DOM/script2.js
+++ JavaScript/DOM/script2.js
@@ -2,11 +2,15 @@
 
 // 1) CREATE THE ELEMENT
 const newh1 = document.createElement("h1");
+const new_a = document.createElement("a");
 
 // 2) ADD PROPERTIES
 newh1.textContent = "Halwa hai kya!";
 newh1.style.color = "purple";
 newh1.style.fontFamily = "monospace";
+
+new_a.href = "https://www.google.com";
+new_a.textContent = "google";
 
 // 3) APPEND ELEMENT TO DOM 
 // document.body.append(newh1);                        // to add it as the last child
@@ -14,6 +18,7 @@ newh1.style.fontFamily = "monospace";
 
 // To add it before a specific element/child~
 document.body.insertBefore(newh1, box2);
+document.body.insertBefore(new_a, box2);
 
 // 4) REMOVE THE ELEMENT
 // document.getElementById("box1").removeChild(newh1);

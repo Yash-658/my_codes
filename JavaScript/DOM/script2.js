1cb1c36a1957ddbd815472b25e3242586fa731ca
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-09 19:17:29 +0530

    .

--- JavaScript/DOM/script2.js
+++ JavaScript/DOM/script2.js
@@ -1,4 +1,4 @@
-// ________EXAMPLE1 <h1>________
+// ________EXAMPLE1 <h1> and <a>________
 
 // 1) CREATE THE ELEMENT
 const newh1 = document.createElement("h1");
@@ -25,7 +25,7 @@ document.body.insertBefore(new_a, box2);
 
 // -------------------------------------------------------------------------------------------------------
 // eventListener = Listen for specific events to create interactive web pages events~
-//                 events: click, mouseover, mouseout       (we use addEventListener(event, callback/arrow_fxn))
+//                 events: click, mouseover, mouseout, keyup, keydown       (we use addEventListener(event, callback/arrow_fxn))
 
 const myBox = document.getElementById("myBox");
 myBox.addEventListener("mouseover", event => {
@@ -45,3 +45,44 @@ myBox.addEventListener("click", event => {
     myBox.textContent = "ahhh! 🫠😫"; 
     document.body.style.backgroundColor = "#FF1D8D";
 });
+
+// --------------------KEY EVENTS--------------------------
+const ourBox = document.getElementById('ourBox');
+
+document.addEventListener("keydown", event => {        // this is to change color and textContent no matter what key is pressed
+    ourBox.textContent = "🫠";
+    // ourBox.style.backgroundColor = "lightgreen";
+})
+
+document.addEventListener("keyup", event => {
+    ourBox.textContent = "😋";
+    // ourBox.style.backgroundColor = "aquamarine";
+})
+
+let x = 0, y = 0, moveAmount = 10;
+
+document.addEventListener("keydown", (event) => {
+    // console.log(event.key);
+
+    event.preventDefault();        // this is used so that the arrow up and down keys doesn't do the default scrolling behaviour
+
+    if(event.key == 'ArrowUp' && y >= 0) {
+        y -= moveAmount;
+    }
+
+    if(event.key == 'ArrowDown') {
+        y += moveAmount;
+    }
+
+    if(event.key == 'ArrowLeft' && x >= 0) {
+        x -= moveAmount;
+    }
+
+    if(event.key == 'ArrowRight') {
+        x += moveAmount;
+    }
+
+    ourBox.style.top = String(`${y}px`);
+    ourBox.style.left = String(`${x}px`);
+})
+

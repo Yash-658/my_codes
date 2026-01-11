10a4f101efc4760cf6f47040dbb6938a2c6d1e98
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-11 12:58:32 +0530

    .

---
+++ JavaScript/ImageSlider/script.js
@@ -0,0 +1,27 @@
+const images = ["images\\2151138941.jpg", "images\\2151955582.jpg", "images\\2151955588.jpg"];
+const image = document.getElementById("image");
+let pos = 0;
+
+function change(request) {
+    if(request === "previous") {
+        if(pos === 0) {
+            pos = 2;
+        }
+
+        else{
+            pos--;
+        }
+    }
+
+    else{
+        if(pos === 2) {
+            pos = 0;
+        }
+
+        else{
+            pos++;
+        }
+    }
+
+    image.src = images[pos];
+}

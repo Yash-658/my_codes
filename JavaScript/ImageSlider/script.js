f14f9295acc55f649fbf4784f76109b848d559fc
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-11 19:12:24 +0530

    .

--- JavaScript/ImageSlider/script.js
+++ JavaScript/ImageSlider/script.js
@@ -2,7 +2,12 @@ const images = ["images\\2151138941.jpg", "images\\2151955582.jpg", "images\\215
 const image = document.getElementById("image");
 let pos = 0;
 
+let autoChange = setInterval(() => {
+    change("auto")
+}, 2000);
+
 function change(request) {
+    if(request !== "auto") clearInterval(autoChange);
     if(request === "previous") {
         if(pos === 0) {
             pos = 2;

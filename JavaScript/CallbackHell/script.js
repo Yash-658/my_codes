548236cb51b138daa93ee5a9e43abf5ea0d89e8e
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-11 21:18:30 +0530

    .

---
+++ JavaScript/CallbackHell/script.js
@@ -0,0 +1,6 @@
+/* Callback Hell: Situation in JS where callbacks are nested within other callbacks 
+                  to the degree where the code is difficult to read.
+                  Modern techs include Promises + async/await to avoid callback hell.
+
+
+*/

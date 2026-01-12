aa0393efdfac572cddc7d5202aedbf65baf5dd79
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-12 21:09:29 +0530

    .

--- JavaScript/JSON, fetch/script.js
+++ JavaScript/JSON, fetch/script.js
@@ -8,9 +8,37 @@ JSON: (JavaScript Object Notation) data-interchaneg format used for exchanging d
     .json() = method is used to parse JSON-formatted data into a native object.
 */
 
+// for fetching from a local JSON file~
 
-fetch("people.json")
-    .then(response => response.json())
-    .then(values => values.forEach(value => console.log("name: " + value.name)))
-    .then(() => console.log("Completed Successfully!"))
-    .catch(error => console.error(error))
+// fetch("people.json")
+//     .then(response => {
+//         if (!response.ok) throw new Error("Failed to fetch data!");
+//         return response.json();
+//     })
+//     .then(values => values.forEach(value => console.log("name: " + value.name)))
+//     .then(() => console.log("Completed Successfully!"))
+//     .catch(error => console.error(error))
+
+// for fetching from a pokemon api, and using async/await instead of .then()
+
+const input = document.getElementById("input");
+const imgEl = document.getElementById("imgEl");
+
+async function fetchPokemon() {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value.toLowerCase()}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch resources!");
+        }
+
+        const data = await response.json();
+        console.log(data.name);
+        imgEl.src = data.sprites.front_default;
+        imgEl.style.display = "block";
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+

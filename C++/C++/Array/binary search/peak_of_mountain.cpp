a0512bf5fa9ca7273ecaed32783572f059fb641d
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-22 08:58:02 +0530

    .

--- C++/C++/Array/binary search/peak_of_mountain.cpp
+++ C++/C++/Array/binary search/peak_of_mountain.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {     // hmesha peak ke liye (mid+1) check krna 
-        int s=1, l=(size(arr)-2);   
+        int s=1, l=arr.size()-2;   
         while(s<l)
         {
             int mid = (s+l)/2;

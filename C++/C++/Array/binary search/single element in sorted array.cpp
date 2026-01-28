2b2b32ee6c98bc0ef08ebfcc590c8085bdd143c0
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-01-28 08:51:06 +0530

    .

---
+++ C++/C++/Array/binary search/single element in sorted array.cpp
@@ -0,0 +1,9 @@
+/*
+
+IMP OBSERVATION: 
+
+1) length of array will always be odd
+2) single element will always be on an evene index
+
+https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+*/

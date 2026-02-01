960de6be92f773818ab92b69284654a1e041bdb3
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-02-01 10:16:38 +0530

    .

--- C++/C++/Array/Array 1/Set Matrix Zeroes.cpp
+++ C++/C++/Array/Array 1/Set Matrix Zeroes.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/set-matrix-zeroes/
+
 // TC: O(mn) SC: O(m+n) 
 // This is better approach than just zeroing every row and col when you see a zero, 
 // cuz SC would be O(mn) cuz we will have to make a copy of current matrix in that solution~ (if done inplace it would lead to cascading zeroes)
@@ -30,3 +32,62 @@
 //         }
 //     }
 // };
+
+/*
+MOST OPTIMAL SOLUTION: 
+“Use the first row and column as a notice board — but keep two sticky notes to remember if the board itself must be erased.”
+
+Why (0,0) is special:
+It belongs to both first row and first column → cannot store two flags, so use booleans.
+
+TC: O(mn) SC: O(1)
+	​
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        bool first_col = false, first_row = false;
+
+        //for first col~
+        for(int i = 0; i < m; i++) {
+            if(mat[i][0] == 0) first_col = true;
+        }
+
+        //for first row~
+        for(int j = 0; j < n; j++) {
+            if(mat[0][j] == 0) first_row = true;
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                // if 0, set first col and first row to 0 as flags
+                if(mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
+                }
+            }
+        }
+
+        // updating the matrix~
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(mat[i][0] == 0 || mat[0][j] == 0) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+
+        if(first_col) {
+            for(int i = 0; i < m; i++) {
+                mat[i][0] = 0 ;
+            } 
+        }
+
+        if(first_row) {
+            for(int j = 0; j < n; j++) {
+                mat[0][j] = 0;
+            }
+        }
+    }
+};
+*/

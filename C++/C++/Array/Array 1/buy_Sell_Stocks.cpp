b373edca52419b1a8ad31f4717e3783de07e0e46
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-02-04 08:08:15 +0530

    .

--- C++/C++/Array/Array 1/buy_Sell_Stocks.cpp
+++ C++/C++/Array/Array 1/buy_Sell_Stocks.cpp
@@ -1,19 +1,23 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int maxProfit = 0, buyDay = 0;
-        for(int sellDay = 1; sellDay < prices.size(); sellDay++) {
-            int profit = prices[sellDay] - prices[buyDay];
-            if(profit < 0) {
-                buyDay = sellDay;
-            }
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1906365174/
+// TC: O(n) SC: O(1)
+// intution: agr profit -ve aa rha hai that means buyDay se sellDay pe price km tha, and aage dekhne pr bhi sellDay pe khridne pr zyada profit hoga~
 
-            else {
-                // profit is +ve
-                maxProfit = max(profit, maxProfit);
-            }
-        }
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int maxProfit = 0, buyDay = 0;
+//         for(int sellDay = 1; sellDay < prices.size(); sellDay++) {
+//             int profit = prices[sellDay] - prices[buyDay];
+//             if(profit < 0) {
+//                 buyDay = sellDay;
+//             }
 
-        return maxProfit;
-    }
-};
+//             else {
+//                 // profit is +ve
+//                 maxProfit = max(profit, maxProfit);
+//             }
+//         }
+
+//         return maxProfit;
+//     }
+// };

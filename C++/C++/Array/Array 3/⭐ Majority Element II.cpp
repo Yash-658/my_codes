44712e3919becf067264bb16edf3dd54a03f3e9f
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-02-08 11:37:58 +0530

    .

--- C++/C++/Array/Array 3/⭐ Majority Element II.cpp
+++ C++/C++/Array/Array 3/⭐ Majority Element II.cpp
@@ -1,7 +1,23 @@
+// https://leetcode.com/problems/majority-element-ii/
+
 // Approach1: use hashmap O(n) O(n)
 // Approach2: use sorting O(nlogn) O(1)
 
 // Boyer–Moore (n/3) solution.
+
+// Mental model to remember forever 🧠
+// n/2:
+// Cancel pairs → one survivor
+
+// n/3:
+// Cancel triples → two survivors
+
+// General form:
+// For > n/k → track k-1 candidates
+
+// “We generalize Boyer–Moore. Any element appearing more than n/3 times cannot be completely eliminated by removing groups of three distinct elements. 
+// Since there can be at most two such elements, we maintain two candidates and cancel triples. A final pass verifies them.”
+
 // class Solution {
 // public:
 //     vector<int> majorityElement(vector<int>& nums) {

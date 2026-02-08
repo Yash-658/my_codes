d7e63cda793c4bc14a9851f2f0229fcac19ed48a
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-02-08 11:32:34 +0530

    n/3 moore's algo for majority element~

---
+++ C++/C++/Array/Array 3/Majority Element II.cpp
@@ -0,0 +1,45 @@
+// Approach1: use hashmap O(n) O(n)
+// Approach2: use sorting O(nlogn) O(1)
+
+// Boyer–Moore (n/3) solution.
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         // there's no gurantee if a majority element exist, there can be 0,1 or 2 elements with freq > n/3
+
+//         int elem1 = 0, elem2 = 0, count1 = 0, count2 = 0, n = nums.size();
+
+//         for(int &num: nums) {
+//             if(num == elem1) count1++;
+
+//             else if(num == elem2) count2++;
+
+//             else if(count1 == 0) {
+//                 elem1 = num;
+//                 count1++;
+//             }
+
+//             else if(count2 == 0) {
+//                 elem2 = num;
+//                 count2++;
+//             }
+
+//             else{
+//                 count1--; count2--;
+//             }
+//         }
+
+//         // for verification~ 
+
+//         count1 = 0, count2 = 0;
+//         for(int &num: nums) {
+//             if(num == elem1) count1++;
+//             else if(num == elem2) count2++;
+//         }
+
+//         vector<int> ans;
+//         if(count1 > n/3) ans.push_back(elem1);
+//         if(count2 > n/3) ans.push_back(elem2);
+//         return ans;
+//     }
+// };

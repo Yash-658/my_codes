c408afb75a87b1b93829251aab9024bf5126e5e6
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-02-23 09:55:23 +0530

    .

--- C++/C++/Array/Array 4/longest_subarr_with_sum_k.cpp
+++ C++/C++/Array/Array 4/longest_subarr_with_sum_k.cpp
@@ -24,7 +24,7 @@
 
 //         // now store the prefix sum, but first check if there already exists a same prefixSum~
 //         if(prefixSum.find(sum) == prefixSum.end()) {           // understand how we handled duplicate prefixSum
-//             prefixSum[sum] = i;
+//             prefixSum[sum] = i;                                // we are only storing the first occurance of the prefixSum
 //         }
 //     }
 

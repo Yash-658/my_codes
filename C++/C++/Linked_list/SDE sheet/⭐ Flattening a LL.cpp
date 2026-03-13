fae43108ac10a42b6ce999f7858f7152b4934b90
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-03-13 20:22:49 +0530

    .

--- C++/C++/Linked_list/SDE sheet/⭐ Flattening a LL.cpp
+++ C++/C++/Linked_list/SDE sheet/⭐ Flattening a LL.cpp
@@ -1,3 +1,72 @@
+// MOST ELEGANT: Divide & conquer (like merge sort, merging in pairs) TC: O(K log N)  SC: O(log N)
+
+// Node* mergeLL(Node* first, Node* second) {
+// 	Node* dummy = new Node(-1), *temp = dummy;
+
+// 	while(first && second) {
+// 		if(first->data > second->data) {
+// 			temp->child = second;
+// 			second = second->child;
+// 		}
+
+// 		else{
+// 			temp->child = first;
+// 			first = first->child;
+// 		}
+
+// 		temp = temp->child;
+// 		temp->next = NULL;
+// 	}
+
+// 	while(first) {
+// 		temp->child = first;
+// 		temp = temp->child;
+// 		temp->next = NULL;
+// 		first = first->child;
+// 	}
+
+// 	while(second) {
+// 		temp->child = second;
+// 		temp = temp->child;
+// 		temp->next = NULL;
+// 		second = second->child;
+// 	}
+
+// 	temp = dummy->child;
+// 	delete dummy;
+// 	return temp;
+// }
+
+// Node* findMiddle(Node* head) {
+// 	Node* slow = head, *fast = head;
+
+// 	while(fast->next && fast->next->next) {
+// 		fast = fast->next->next;
+// 		slow = slow->next;
+// 	}
+
+// 	return slow;
+// }
+
+// Node* flattenLinkedList(Node* head) 
+// {
+// 	if(!head || !head->next) return head;
+
+// 	// find middle~
+// 	Node* mid = findMiddle(head);
+
+// 	// split~
+// 	Node* second = mid->next;
+// 	mid->next = NULL;
+
+// 	Node* first = flattenLinkedList(head);
+// 	second = flattenLinkedList(second);
+
+// 	// now merge~
+// 	return mergeLL(first, second);
+// }
+
+
 // This is Sequential merging, O(K * N) O(1)    >> here K = total no. of nodes~
 
 // Node* mergeLL(Node* first, Node* second) {
@@ -52,4 +121,3 @@
 // }
 
 
-// Divide & conquer (like merge sort, merging in pairs) O(K log N)

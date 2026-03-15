272ba9e2257d6bb8a411e3db3fac409d75bf8a6c
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-03-15 09:39:44 +0530

    .

--- C++/C++/Linked_list/Clone LL with random nodes.cpp
+++ C++/C++/Linked_list/Clone LL with random nodes.cpp
@@ -7,6 +7,45 @@ IMP
 3) VERY GOOD LOGIC, changing links, TC = O(n), SC = O(1),, most optimised solution,, read 2) first before trying this one~
 
 You have done all three here: https://leetcode.com/problems/copy-list-with-random-pointer/
+*/
 
+/*
+3rd solution: 
 
+Node* copyRandomList(Node* head) {
+    if(!head) return NULL;
+
+    Node* curr = head;
+
+    // 1. Insert copy nodes
+    while(curr) {
+        Node* copy = new Node(curr->val);
+        copy->next = curr->next;
+        curr->next = copy;
+        curr = copy->next;
+    }
+
+    // 2. Assign random pointers
+    curr = head;
+    while(curr) {
+        if(curr->random)
+            curr->next->random = curr->random->next;
+        curr = curr->next->next;
+    }
+
+    // 3. Separate lists
+    curr = head;
+    Node* newHead = head->next;
+
+    while(curr) {
+        Node* copy = curr->next;
+        curr->next = copy->next;
+        if(copy->next)
+            copy->next = copy->next->next;
+
+        curr = curr->next;
+    }
+
+    return newHead;
+}
 */

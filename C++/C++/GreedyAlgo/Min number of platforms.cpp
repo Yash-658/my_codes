e4dcc3a427343f21eae0b4ab86dcd818a0b7151a
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-03-21 23:54:31 +0530

    .

--- C++/C++/GreedyAlgo/Min number of platforms.cpp
+++ C++/C++/GreedyAlgo/Min number of platforms.cpp
@@ -1,31 +1,82 @@
 // https://takeuforward.org/plus/dsa/problems/minimum-number-of-platforms-required-for-a-railway?source=strivers-sde-sheet
 
-// TC: O(n2) SC: O(1)  as we are using a loop to check which platform is free everytime~
+// TC: O(nlogn) SC: O(n)
 
-// class Solution{
+// struct comp{
 //     public:
-//     int findPlatform(vector<int>& Arrival, vector<int>& Departure){
-//         vector<int> platforms;   // its size will tell us how many platforms and its values will tell us each platform's freeTime~
-
-//         platforms.push_back(-1);   // as first platform will always be free at the starting
-
-//         for(int i = 0; i < Arrival.size(); i++) {
-//             bool allocated = false;
-//             for(int &freeTime: platforms) {
-//                 if(freeTime < Arrival[i]) {
-//                     allocated = true;
-//                     freeTime = Departure[i];    // update freeTime of this platform~
-//                     break;
-//                 }
-//             }
-
-//             // if not allocated still, create a new platform and allocate it their~
-//             if(!allocated) platforms.push_back(Departure[i]);
-//         }
-
-//         return platforms.size();
+//     bool operator()(pair<int,int> a, pair<int,int> b) {
+//         return a.first > b.first;
 //     }
 // };
 
-// OPTIMAL APPROACH: TC: O(nlogn) SC: O(n)
-// instead of a vector, if we used a minHeap, we could directly tell the platform which is going to be available the earliest, and if we need to create a new one~
+// class Solution{
+//     public:
+//     int findPlatform(vector<int>& arr, vector<int>& dep){
+//        priority_queue<pair<int,int>, vector<pair<int,int>>, comp> trains;
+        
+//         for(int i = 0; i < arr.size(); i++) {
+//             trains.push({arr[i],dep[i]});
+//         }
+        
+//         // now we have trains arr and dep in a sorted manner~
+//         // just need to calculate at any particular time what's the min max platforms will be used~
+        
+//         int ans = 1;
+        
+//         priority_queue<int, vector<int>, greater<int>> currQ;
+
+//         while(!trains.empty()) {
+//             auto [currArr, currDep] = trains.top();
+//             trains.pop();
+            
+//             if(currQ.empty()) {
+//                 currQ.push(currDep);
+//             }
+            
+//             else{
+//                 while(!currQ.empty() && currQ.top() < currArr) {
+//                     currQ.pop();
+//                 }
+                
+//                 currQ.push(currDep);
+//                 ans = max(ans, (int)currQ.size());
+//             }
+//         }
+        
+//         return ans;
+        
+//     }
+// };
+
+// OPTIMAL APPROACH: TC: O(nlogn) SC: O(1)
+// Instead of pairing trains, treat:
+
+// all arrivals as +1 platform needed
+// all departures as -1 platform freed
+
+// 👉 Then process them in time order
+
+// int findPlatform(vector<int>& arr, vector<int>& dep) {
+//     sort(arr.begin(), arr.end());
+//     sort(dep.begin(), dep.end());
+
+//     int i = 0, j = 0;
+//     int platforms = 0, ans = 0;
+
+//     while(i < arr.size()) {
+//         if(arr[i] <= dep[j]) {                                // used <= as according to question if a train arrived at the same time one leaves, it will still need a new platform
+//             platforms++;   // new train arrives
+//             ans = max(ans, platforms);
+//             i++;
+//         }
+//         else {
+//             platforms--;   // train departs
+//             j++;
+//         }
+//     }
+
+//     return ans;
+// }
+
+// comp(a, b) = true means:
+// “a should come after b” (a has lower priority)

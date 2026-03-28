6e6bb37a9027659fa65be67368c5ffcd9a0cd532
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-03-29 00:10:24 +0530

    Cs1

---
+++ C++/C++/Recursion/CombinationSum 1.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/combination-sum/
+
+// quickies: how did I used ind parameter to remove duplicate subsets?
+
+// class Solution {
+// public:
+//     void solve(int ind, int curr, const vector<int>& nums, int target, vector<int> &temp, vector<vector<int>> &ans) {
+//         int n = nums.size();
+        
+//         for(int i = ind; i < n; i++){
+//             if(curr + nums[i] == target) {
+//                 temp.push_back(nums[i]);
+//                 ans.push_back(temp);
+//                 temp.pop_back();
+//                 return;
+//             }
+
+//             else if(curr + nums[i] < target) {
+//                 temp.push_back(nums[i]);
+//                 solve(i, curr + nums[i], nums, target, temp, ans);
+//                 temp.pop_back();
+//             }
+
+//             else{
+//                 return;
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         sort(candidates.begin(), candidates.end());
+
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         solve(0, 0, candidates, target, temp, ans);
+//         return ans;
+//     }
+// };

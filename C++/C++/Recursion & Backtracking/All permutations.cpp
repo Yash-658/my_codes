0613a4015ac3ca00a4eed960c57daf5cb2d270ed
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-03-31 09:55:17 +0530

    permutations

---
+++ C++/C++/Recursion & Backtracking/All permutations.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/permutations/submissions/1675920009/
+
+// make recursion tree if can't understand~
+// TC: O(n!) auxillary TC: O(n)
+
+// class Solution {
+// public:
+//     void solve(int ind, vector<int>& nums, vector<vector<int>> &ans) {
+//         if(ind >= nums.size()) {
+//             ans.push_back(nums);
+//             return;
+//         }
+
+//         for(int i = ind; i < nums.size(); i++) {
+//             swap(nums[ind], nums[i]);
+//             solve(ind+1, nums, ans);
+//             swap(nums[ind], nums[i]);
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         solve(0, nums, ans);
+//         return ans;
+//     }
+// };

60433bdcd2c9b3cd3643a560af12f96925eee23a
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-04-05 14:45:40 +0530

    dp3

--- C++/C++/DP/WordBreak.cpp
+++ C++/C++/DP/WordBreak.cpp
@@ -6,7 +6,7 @@
 // O(n²)
 
 // And each index is processed
-// 👉 so total = O(n³), can optimise a little more if we limit substring length using max word length present in the Dict~  then it will be O(n² * (max_word_length_in_dict))
+// 👉 so total = O(n³) MORE OPTIMISED VERSION BELOW
 
 // class Solution {
 // public:
@@ -41,5 +41,46 @@
 
 
 //         return isPossible(0, s, n, ans, dict);
+//     }
+// };
+
+
+//    ⭐⭐⭐ Memoization with max word optimization 👉 O(n × L²)
+//     where L = max word length
+
+// class Solution {
+// public:
+
+//     bool isPossible(int start, const string &s, int maxLength, vector<int> &ans, const unordered_set<string> &dict) {
+//         // base case 
+//         if(start == s.size()) return true;
+
+//         // check if we already computed with this as the start index~
+//         if(ans[start] != -1) return ans[start];
+
+//         string temp = "";
+//         for(int curr = start; curr < min(start+maxLength, (int)s.size()); curr++) {
+//             temp.push_back(s[curr]);
+//             if(dict.contains(temp) && isPossible(curr+1, s, maxLength, ans, dict)) {
+//                 return ans[start] = true;
+//             }
+//         }
+
+//         return ans[start] = false;
+//     }
+
+//     bool wordBreak(string s, vector<string>& wordDict) {
+//         int n = s.size();
+//         unordered_set<string> dict;
+//         vector<int> ans(n, -1);     // ans[i] will have 3 values, -1 if not computed, 0 if false, 1 if true
+
+//         int maxLength = 0;
+
+//         for(const string &str: wordDict) {
+//             maxLength = max(maxLength, (int)str.size());
+//             dict.insert(str);
+//         }
+
+//         return isPossible(0, s, maxLength, ans, dict);
 //     }
 // };

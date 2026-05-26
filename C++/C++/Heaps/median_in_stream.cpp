290ee5694979ed8805b11f8b314125b3f4544cc0
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-05-26 18:47:13 +0530

    .

--- C++/C++/Heaps/median_in_stream.cpp
+++ C++/C++/Heaps/median_in_stream.cpp
@@ -1,117 +1,59 @@
-// THIS IS THE BRUTE FORCE APPROACH WITH INSERTION SORT, O(n^2)
+// https://leetcode.com/problems/find-median-from-data-stream/
 
-// void insertionSort(int n, vector<int> &arr){                  
-//     int temp, j;
-//     for(int i = 1; i < n; i++)                             
-//     {                                                         
-//         temp = arr[i];
-//         j = (i-1);
+// class MedianFinder {
+// public:
+//     priority_queue<int> maxHeap;
+//     priority_queue<int, vector<int>, greater<int>> minHeap;
 
-//         while(j>=0)
-//         {
-//             if(arr[j]>temp)
-//             {
-//                 arr[j+1] = arr[j];
-//                 j--;
+//     MedianFinder() {
+//     }
+    
+//     void addNum(int num) {
+//         if(maxHeap.empty()) {
+//             maxHeap.push(num);
+//             return;
+//         }
+
+//         // comparing values with top elements in both heaps~ 
+//         int max = maxHeap.top();
+        
+//         if(maxHeap.size() == minHeap.size()) {
+//             int min = minHeap.top();
+//             if(max < num) {
+//                 minHeap.pop();
+//                 minHeap.push(num);
+//                 maxHeap.push(min);
 //             }
-             
-//             else
-//             {
-//                 break;
+
+//             else{
+//                 maxHeap.push(num);
 //             }
 //         }
 
-//         arr[j+1] = temp;
+//         else{
+//             if(num <= max) {
+//                 maxHeap.pop();
+//                 maxHeap.push(num);
+//                 minHeap.push(max);
+//             }
+
+//             else{
+//                 minHeap.push(num);
+//             }
+//         }
 //     }
-// }
+    
+//     double findMedian() {
+//         double ans;
+//         if(maxHeap.size() != minHeap.size()) {
+//             ans = maxHeap.top();
+//         }
 
-// vector<int> findMedian(vector<int> &arr, int n){
-	
-// 	vector<int> res;
-// 	for(int i = 1; i <= n; i++) {
-// 		insertionSort(i, arr);
-// 		if(i&1) {
-// 			res.push_back(arr[i/2]);
-// 		}
+//         else{
+//             ans = (maxHeap.top() + minHeap.top()) / 2.0;
+//         }
 
-// 		else {
-// 			int temp = (arr[i/2] + arr[i/2 - 1])/2;
-// 			res.push_back(temp);
-// 		}
-// 	}
+//         return ans;
+//     }
+// };
 
-// 	return res;
-// }
-
-
-/* THIS IS THE OPTIMISED VERSION USING TWO HEAPS O(NlogN)
-#include<queue>
-
-int signum(int a, int b) {
-	if(a == b) return 0;
-	if(a > b) return 1;
-	else return -1;
-}
-
-void getMedian(int element, priority_queue<int> &maxHeap, 
-priority_queue<int, vector<int>, greater<int>> &minHeap, int &median) {
-
-	switch(signum(maxHeap.size(), minHeap.size())) {
-		case 0: 
-		if(element > median) {
-			minHeap.push(element);
-			median = minHeap.top();
-		}
-
-		else {
-			maxHeap.push(element);
-			median = maxHeap.top();
-		}
-
-		break;
-
-		case 1:
-		if(element > median) {
-			minHeap.push(element);
-			median = (minHeap.top() + maxHeap.top())/2;
-		}
-
-		else {
-			minHeap.push(maxHeap.top());
-			maxHeap.pop();
-			maxHeap.push(element);
-			median = (minHeap.top() + maxHeap.top())/2;
-		}
-		break;
-
-		case -1:
-		if(element > median) {
-			maxHeap.push(minHeap.top());
-			minHeap.pop();
-			minHeap.push(element);
-			median = (minHeap.top() + maxHeap.top())/2;
-		}
-
-		else {
-			maxHeap.push(element);
-			median = (minHeap.top() + maxHeap.top())/2;
-		}
-		break;
-	} 
-}
-
-vector<int> findMedian(vector<int> &arr, int n){
-	vector<int> res;
-	priority_queue<int> maxHeap;
-	priority_queue<int, vector<int>, greater<int>> minHeap;
-	int median = 0;
-
-	for(int i = 0; i < n; i++) {
-		getMedian(arr[i], maxHeap, minHeap, median);
-		res.push_back(median);
-	}
-
-	return res;
-}
-
-*/

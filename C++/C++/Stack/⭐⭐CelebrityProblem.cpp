f686706f168a00ee85018a95a4aca78108cd6ed5
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-05-31 17:13:12 +0530

    .

--- C++/C++/Stack/⭐⭐CelebrityProblem.cpp
+++ C++/C++/Stack/⭐⭐CelebrityProblem.cpp
@@ -14,26 +14,17 @@
 //                 top++;
 //             }
             
-//             else if(mat[bottom][top] == 1) {
-//                 bottom--;
-//             }
-            
 //             else {
-//                 top++;
 //                 bottom--;
 //             }
 //         }
-        
-//         if(top > bottom) return -1;
-//         else {
-//             // top == bottom, potential canditate
-//             for(int i = 0; i < n; i++) {
-//                 if(top == i) continue;
-//                 if(mat[top][i] == 1) return -1;
-//                 if(mat[i][top] == 0) return -1;
-//             }
+
+//         // top == bottom, potential canditate
+//         for(int i = 0; i < n; i++) {
+//             if(top == i) continue;
+//             if(mat[top][i] == 1) return -1;
+//             if(mat[i][top] == 0) return -1;
 //         }
-        
 //         return top;
 //     }
 // };

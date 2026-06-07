87c9e4ca01d0849fa8c9800951e8ca9a2ba2c020
Yash Raghuwanshi <136227872+Yash-658@users.noreply.github.com>
2026-06-07 15:15:56 +0530

    .

---
+++ C++/C++/Strings/Longest Palindrome in a string.cpp
@@ -0,0 +1,76 @@
+// https://leetcode.com/problems/longest-palindromic-substring/submissions/2025218180/
+
+
+// I HAVE NOT DONE THE MOST OPTIMIZED VERSION: Manacher's Algorithm O(n) O(1)
+
+
+// INTUTION: Every palindrome is defined by its center. Instead of checking every substring, 
+// treat each index (and each gap between indices) as a center and expand outward while the characters match.
+// The longest expansion across all centers is the answer.
+
+// OPTIMIZED O(N^2) O(1)
+
+// class Solution {
+// public:
+//     void expand(int left, int right, string &s, int &start, int &maxLen) {
+//         int n = s.size();
+
+//         while (left >= 0 && right < n && s[left] == s[right]) {
+//             left--;
+//             right++;
+//         }
+
+//         // palindrome is from left+1 to right-1
+//         int currLen = right - left - 1;
+
+//         if (currLen > maxLen) {
+//             maxLen = currLen;
+//             start = left + 1;
+//         }
+//     }
+
+//     string longestPalindrome(string s) {
+//         int n = s.size();
+
+//         int start = 0;
+//         int maxLen = 1;
+
+//         for (int i = 0; i < n; i++) {
+//             expand(i, i, s, start, maxLen);       // odd length
+//             expand(i, i + 1, s, start, maxLen);   // even length
+//         }
+
+//         return s.substr(start, maxLen);
+//     }
+// };
+
+// brute force O(n^3) O(1)
+
+// class Solution {
+// public:
+//     string longestPalindrome(string s) {
+//         string ans;
+//         int n = s.size();
+//         for(int i = 0; i < n; i++) {
+//             string temp;
+//             for(int j = i; j < n; j++) {
+//                 temp.push_back(s[j]);
+//                 // check if palindrome, if valid, check if new ans~
+//                 int low = 0, high = temp.size()-1;
+//                 bool isValid = true;
+//                 while(low < high) {
+//                     if(temp[low++] != temp[high--]) {
+//                         isValid = false;
+//                         break;
+//                     }
+//                 }
+
+//                 if(isValid && temp.size() > ans.size()) {
+//                     ans = temp;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
